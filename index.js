3322083a50d3c270511da840745330c43f704dbe
Quang Nguyen Minh <quangg.nguyenminh@gmail.com>
2018-12-08 21:50:05 +0700

    Complete

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var path = require("path")
+var favicon = require('serve-favicon')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+app.use(favicon(path.join(__dirname + '/public/images/favicon.png')))
 
 app.get('/', function(request, response) {
   res.sendFile(path.join(__dirname + '/public/index.html'))
