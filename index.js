738216ffa199263f3d3ce893258ffd13e311d27d
Quang Nguyen Minh <quangg.nguyenminh@gmail.com>
2018-12-10 11:07:20 +0700

    Test Helmet X-XSS-Protection

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var path = require("path")
 var favicon = require('serve-favicon')
+const helmet = require('helmet')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 app.use(favicon(path.join(__dirname + '/public/images/favicon.png')))
+// Sets "X-XSS-Protection: 1; mode=block" on all versions of IE
+app.use(helmet.xssFilter({ setOnOldIE: true }))
 
 app.get('/', function(request, response) {
   res.sendFile(path.join(__dirname + '/public/index.html'))
